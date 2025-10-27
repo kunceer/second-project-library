eec4c24c47b4815eb4f3b2362fdc5c46ee08e8ca
kunceer <llara.kuncer@gmail.com>
2025-10-27 14:10:40 +0100

    last update for monday, register,(form and route)

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,5 +8,26 @@ router.get("/register", (req, res)=> {
     res.render("users/register.ejs");
 
 });
+router.post("/register",async(req,res)=> {
+    const {name,email,password}=req.body
+    try{
+        const exist=await User.findOne({email})
+        if (exist)
+            return res.render("users/register.ejs", { error: "Email already in use"});
+
+        const user = new User({
+            name,
+            email,
+            password
+
+        })
+        await user.save();
+        res.redirect("/")
+        
+    } catch (err) {
+        console.error(err);
+        res.render("register.ejs", {error: "Something went wrong"});
+    }
+})
 
 module.exports=router;
